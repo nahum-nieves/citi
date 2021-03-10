7ef2e7a6bb489d2bff8dd46d7eb8fe717eceb62a
Nahum Nieves <anieves@nxtlab.mx>
2021-03-09 19:33:16 -0600

    ADDED LOGGING
    Se añade loggeo y ResponseBody

--- citi/src/main/java/com/citi/api/CitiController.java
+++ citi/src/main/java/com/citi/api/CitiController.java
@@ -3,9 +3,6 @@ package com.citi.api;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import com.citi.services.CitiService;
 
@@ -13,6 +10,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -24,11 +22,13 @@ public class CitiController {
     private final CitiService citiService;
 
     @GetMapping(value = "/headers")
+    @ResponseBody
     public Map<String,List<String>> encontrarHeaders(@RequestHeader HttpHeaders httpHeaders){
         return citiService.encontrarHeaders(httpHeaders);
     }
 
     @GetMapping(value = "/headers-filter")
+    @ResponseBody
     public Map<String,List<String>> filtrarHeaders(@RequestHeader HttpHeaders httpHeaders,@RequestParam(value = "headerNames",required = false) List<String> headerNames){
         Objects.requireNonNull(headerNames, "You have to send headerNames param");
         return citiService.encontrarHeaders(httpHeaders, headerNames);
--- citi/src/main/java/com/citi/services/CitiService.java
+++ citi/src/main/java/com/citi/services/CitiService.java
@@ -8,13 +8,18 @@ import java.util.stream.Collectors;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.java.Log;
+
 @Service
+@Log
 public class CitiService {
 
     public Map<String, List<String>> encontrarHeaders(HttpHeaders httpHeaders) {
+        log.info("Collecting headers");
         return httpHeaders.keySet().stream().collect(Collectors.toMap(Function.identity(),header-> httpHeaders.getValuesAsList(header)));
     }
     public Map<String, List<String>> encontrarHeaders(HttpHeaders httpHeaders, List<String> headerNames) {
+        log.info("Filtering and collecting headers");
         return httpHeaders.keySet().stream().filter(header->headerNames.contains(header))
         .collect(Collectors.toMap(Function.identity(),header-> httpHeaders.getValuesAsList(header)));        
     }
